a3508ec0bc5d7a7043579517d377eb947c272a0d
Mary <149751752+mariesauve@users.noreply.github.com>
2024-09-20 21:29:06 -0400

    First

---
+++ websiteTest/Scripts.js
@@ -0,0 +1,38 @@
+﻿const myBox = document.getElementById("myBox");
+const myButton = document.getElementById("myButton");
+function changeColor(event) {
+    /*console.log(event);*/
+
+    //event.target.style.backgroundColor = "purple"
+    //event.target.textContent = "welcome! ";
+}
+/*myBox.addEventListener("click", changeColor);*/
+
+//myBox.addEventListener("click", function(event) {
+//    event.target.style.backgroundColor = "purple"
+//    event.target.textContent = "welcome! ";
+//});
+
+myButton.addEventListener("click", myBox => {
+    myBox.target.style.backgroundColor = "purple"
+    myBox.target.textContent = "welcome! ";
+});
+
+
+
+//myBox.addEventListener("click", event => {
+//    event.target.style.backgroundColor = "purple"
+//    event.target.textContent = "welcome! ";
+//});
+
+//myBox.addEventListener("mouseover", event => {
+//    event.target.style.backgroundColor = "yellow"
+//    event.target.textContent = "Nooooo! ";
+
+//});
+
+//myBox.addEventListener("mouseout", event => {
+
+//    event.target.style.backgroundColor = "gold"
+//    event.target.textContent = "Hello! ";
+//});
